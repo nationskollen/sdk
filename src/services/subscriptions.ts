ff84f2261552dfdb0fb7cf626e4d55fae227e9df
Fredrik Engstrand <frewacom@gmail.com>
2021-05-11 12:38:30 +0200

    Updated subscription create data type

--- src/services/subscriptions.ts
+++ src/services/subscriptions.ts
@@ -2,12 +2,21 @@ import { BaseService } from './base'
 import { Connection, HttpMethod } from '../connection'
 import { Subscription as SubscriptionResponse } from '../responses'
 
+/**
+ * Defines the required data needed to create a subscription.
+ */
+export interface SubscriptionCreateData {
+    oid: number
+    topic: number
+    token: string
+}
+
 export class Subscriptions extends BaseService {
     constructor(connection: Connection) {
         super(connection)
     }
 
-    public create = async (data: SubscriptionResponse): Promise<SubscriptionResponse> => {
+    public create = async (data: SubscriptionCreateData): Promise<SubscriptionResponse> => {
         const subscription = await this.$connection.request<SubscriptionResponse>(
             HttpMethod.POST,
             `/subscriptions`,
